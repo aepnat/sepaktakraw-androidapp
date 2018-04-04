23b3e2b0c0771b5619c15c7b9a767a504f1eb849
Saepul <aep.nat@gmail.com>
2018-04-04 21:13:29 +0700

    splash screen

--- app/src/main/java/com/saepulbahri/sepaktakrawindustry/SplashScreenActivity.kt
+++ app/src/main/java/com/saepulbahri/sepaktakrawindustry/SplashScreenActivity.kt
@@ -1,12 +1,27 @@
 package com.saepulbahri.sepaktakrawindustry
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splash_timeout = 4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // FullScreen No ActionBar
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed(object : Runnable {
+            public override fun run() {
+                startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+                finish()
+            }
+        },splash_timeout.toLong())
     }
 }
